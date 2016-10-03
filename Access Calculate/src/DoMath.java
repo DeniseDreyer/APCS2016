b719aecb5a031774082fee7173ae86d4990c9c2f
DeniseDreyer <ddreyer@millshighsch.org>
2016-10-02 22:32:35 -0700

    temp convert

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -13,18 +13,25 @@ public class DoMath {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Calculate.square(5));
-		System.out.println(Calculate.cube(5));
-		System.out.println(Calculate.average(5, 4));
-		System.out.println(Calculate.average(5,4,3));
-		System.out.println(Calculate.toDegrees(1.57));
-		System.out.println(Calculate.toRadians(180));
-		System.out.println(Calculate.discriminant(1,-2, 5));
-		System.out.println(Calculate.toImproperFrac(5, 1,3));
-		System.out.println(Calculate.toMixedNum(5,3));
-		System.out.println(Calculate.foil(5, 1,3, -4, "x"));
-
-		
+	//	System.out.println(Calculate.square(5));
+	//	System.out.println(Calculate.cube(5));
+	//	System.out.println(Calculate.average(5, 4));
+	//	System.out.println(Calculate.average(5,4,3));
+	//	System.out.println(Calculate.toDegrees(1.57));
+	//	System.out.println(Calculate.toRadians(180));
+	//	System.out.println(Calculate.discriminant(1,-2, 5));
+	//	System.out.println(Calculate.toImproperFrac(5, 1,3));
+	//	System.out.println(Calculate.toMixedNum(5,3));
+	//	System.out.println(Calculate.foil(5, 1,3, -4, "x"));
+		System.out.println(Calculate.isDivisibleBy(40, 8));
+	//	System.out.println(Calculate.absValue(5));
+	//	System.out.println(Calculate.max(5,-5));
+	//	System.out.println(Calculate.max(4.0, 5.0, -6.0));
+	//	System.out.println(Calculate.min(-5,50));
+	//	System.out.println(Calculate.round2(23.452677));
+	//	System.out.println(Calculate.exponent(-2.0,3));
+	//	System.out.println(Calculate.factorial(4));
+		System.out.println(Calculate.isPrime(4));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -37,6 +37,72 @@ public class Calculate {
 	}
 	public static String foil( int a, int b, int c, int d, String variable){
 		return (a*c+variable+"^2"+(b*c+a*d)+variable + b*d);
+	}
+	public static boolean isDivisibleBy(int num1, int num2){
+		return (num1%num2 == 0);
+	}
+	public static double absValue(double number){
+		if (number >= 0) {
+			return (number);
+		}
+		else 
+			return (-1*number);
+	}
+	public static int max(int num1, int num2){
+		if (num1 > num2){
+			return num1;
+		}
+		else {
+			return num2;
+		}
+	}
+	public static double max(double num1, double num2, double num3){
+		double maxnum = num1;
+		if (num2 > maxnum){
+			maxnum = num2;
+		}
+		if (num3 > maxnum){
+			maxnum = num3;
+		}
+		return maxnum;
+	}
+	public static int min(int num1, int num2){
+		if (num1 < num2){
+			return num1;
+		}
+		else {
+			return num2;
+		}
+	}
+	public static double round2(double number){
+		double answer;
+		int tempAnswer;
+		tempAnswer = (int) (100*(0.005+number));
+		answer = tempAnswer/100.0;
+		return (answer);
+	}
+	public static double exponent(double base, int expo){
+		double result = 1.0;
+		for (int i=1; i<= expo; i++){
+			result = result*base;
+		}
+		return result;
+	}
+	public static int factorial(int startNum){
+		int result=1;
+		for(int i = startNum; i > 1; i--){
+			result = result*i;
+		}
+		return result;
+	}
+	public static boolean isPrime(int number){
 		
+		for (int i = number; i > 1; i --){
+			if (isDivisibleBy(number, i-1)){
+				System.out.println(i);
+				return true;
+			}
+		}
+		return false;
 	}
 }
